2f6b88ce736ffbe6fd623b889325459503cee9b3
MurielPlouis <36716211+MurielPlou@users.noreply.github.com>
2018-04-11 20:56:03 -0400

    After Third Lesson

--- minesweeper.js
+++ minesweeper.js
@@ -1,20 +1,42 @@
-const exampleBoard = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
-];
-
-
-const printBoard = board => {
-  console.log('Current Board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+   const row = [];
+   for (let columnsIndex = 0; columnsIndex < numberOfColumns; columnsIndex++) {
+     row.push(' ');
+   }
+   board.push(row);
+}
+  return board;
 };
 
-printBoard(exampleBoard);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+   const row = [];
+   for (let columnsIndex = 0; columnsIndex < numberOfColumns; columnsIndex++) {
+     row.push(null);
+   }
+   board.push(row);
+}
 
-exampleBoard[0][1] = '1';
-exampleBoard[1][0] = 'B';
+let numberOfBombsPlaced = 0;
+while (numberOfBombsPlaced < numberOfBombs){
+  const randomRowIndex = Math.floor(Math.random() * numberOfRows)
+  const randomColumnIndex = Math.floor(Math.random() * numberOfColumns)
 
-printBoard(exampleBoard);
+  board[randomRowIndex][randomColumnIndex] = 'B';
+
+  numberOfBombsPlaced ++;
+}
+  return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+const playerBoard = generatePlayerBoard(3, 3);
+const bombBoard = generateBombBoard(3, 2, 2);
+
+printBoard(playerBoard);
