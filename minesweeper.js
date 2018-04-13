fc5e855a545f35e4958cea638f27271732e30305
MurielPlouis <36716211+MurielPlou@users.noreply.github.com>
2018-04-13 13:24:07 -0400

    After Lesson 14

--- minesweeper.js
+++ minesweeper.js
@@ -24,19 +24,66 @@ let numberOfBombsPlaced = 0;
 while (numberOfBombsPlaced < numberOfBombs){
   const randomRowIndex = Math.floor(Math.random() * numberOfRows)
   const randomColumnIndex = Math.floor(Math.random() * numberOfColumns)
-
-  board[randomRowIndex][randomColumnIndex] = 'B';
-
-  numberOfBombsPlaced ++;
+  //Check to make sure bombs were not put on top of bombs
+  if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced ++;
+  }
 }
   return board;
 };
 
-const printBoard = board => {
-  console.log(board.map(row => row.join(' | ')).join('\n'));
+const getNumberOfSurroundingBombs = (bombBoard, flipRow, flipColumn) =>{  //create offsets for columns
+  const neighborOffsets = [[0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1]];
+
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+
+  let numberOfSurroundingBombs = 0;
+  neighborOffsets.forEach(neighborOffsets => {
+    const neighborRowIndex = flipRow + neighborOffsets[0];
+    const neighborColumnIndex = flipColumn + neighborOffsets[1];
+//Check to see if row and column are valid tile values on board
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns){
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B'){
+        numberOfSurroundingBombs++;
+      }
+    }
+  });
+
+  return numberOfSurroundingBombs;
+
+}
+
+const flipTile = (playerBoard, bombBoard, flipRow, flipColumn) => {
+  //Check if tile is already flipped, if so return
+  if (playerBoard[flipRow][flipColumn] !== ' ') {
+    return;
+}
+  //Check if tile is bomb, if so place bomb on playerBoard
+  if (bombBoard[flipRow][flipColumn] === 'B') {
+    playerBoard[flipRow][flipColumn] = 'B';
+} else{  //If tile is not bomb, place number of surrounding bombs on playerBoard
+    playerBoard[flipRow][flipColumn] = getNumberOfSurroundingBombs(bombBoard, flipRow, flipColumn);
+}
 };
 
-const playerBoard = generatePlayerBoard(3, 3);
-const bombBoard = generateBombBoard(3, 2, 2);
 
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+const playerBoard = generatePlayerBoard(3, 3);
+const bombBoard = generateBombBoard(3, 3, 3);
+
+printBoard(bombBoard);
+console.log(getNumberOfSurroundingBombs(bombBoard, 0, 0));
+
+/*console.log('PlayerBoard: ');
 printBoard(playerBoard);
+console.log('BombBoard: ');
+printBoard(bombBoard);
+flipTile(playerBoard, bombBoard, 1, 1);
+printBoard(playerBoard);
+printBoard(playerBoard);
+*/
